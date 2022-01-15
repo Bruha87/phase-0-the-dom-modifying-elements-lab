6b1df88cfe1d14d368456c2c4eb8a088a3e63660
willbruha <wbruhaha87@gmail.com>
2022-01-14 21:18:31 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 // Write your code here!
 const main = document.querySelector('#main')
-
 main.remove()
 
-const newHeader = document.createElement('h1')
+const newHeader = document.createElement("h1")
 newHeader.setAttribute("id", "victory")
 
-newHeader.innerHTML = "Will is the champion"
+newHeader.innerHTML = "is the champion"
 
 document.body.append(newHeader)
+
+
+
+
+
+
+
+
+// const element = document.createElement("div");
+// document.body.append(element);
+
+// const ul = document.createElement("ul");
+
+// for (let i = 0; i < 3; i++) {
+//   const li = document.createElement("li");
+//   li.textContent = (i + 1).toString();
+//   ul.append(li);
+// }
+
+// element.append(ul);
+
+
+
+
+// const element = document.getElementById("main");
+// element.style.height = "300px";
+// element.style.backgroundColor = "#27647B";
+
+// element.textContent = "You've changed what's on the screen!";
+
+// element.style.fontSize = "24px";
+// element.style.marginLeft = "30px";
+// element.style.lineHeight = 2;
+
+
+
+
